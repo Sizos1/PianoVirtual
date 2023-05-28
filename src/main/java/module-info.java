8da962453dc1c108a8d22a60945e66ec98ce077d
Sisos <daaguilarc@unal.edu.co>
2023-05-28 12:07:52 -0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.pianovirtual {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.pianovirtual to javafx.fxml;
+    exports com.example.pianovirtual;
+}
